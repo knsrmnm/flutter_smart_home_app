99b911631986ffe97ab1f8c5bdffe7e5f5664632
Ken Minami <kenminami.dev@gmail.com>
2022-03-02 21:46:01 +0900

    Update home_screen.dart

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -9,6 +9,33 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        leading: Icon(Icons.menu, color: Colors.black87),
+        elevation: 0,
+        actions: [
+          Padding(
+            padding: EdgeInsets.only(right: 15.0),
+            child: Row(
+              children: [
+                Icon(Icons.notifications, color: Colors.black87),
+                const SizedBox(width: 15.0),
+                Icon(Icons.notifications, color: Colors.black87),
+              ],
+            ),
+          )
+        ],
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
+          child: SingleChildScrollView(
+
+          ),
+        ),
+      ),
+    );
   }
 }
