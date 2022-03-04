bc9aadd6bbbf27c65508c1e9a32c9146901623a9
Ken Minami <kenminami.dev@gmail.com>
2022-03-04 23:23:47 +0900

    Update home_screen.dart

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -22,7 +22,7 @@ class _HomeScreenState extends State<HomeScreen> {
               children: [
                 Icon(Icons.notifications, color: Colors.white),
                 const SizedBox(width: 15.0),
-                Icon(Icons.notifications, color: Colors.white),
+                Icon(Icons.info_outline, color: Colors.white),
               ],
             ),
           )
@@ -43,8 +43,25 @@ class _HomeScreenState extends State<HomeScreen> {
                     fontFamily: 'Comfortaa'
                   ),
                 ),
-                Text("Rooms", style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.w500, color: Colors.grey, fontFamily: 'Comfortaa')),
-                Text("Devices", style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.w500, color: Colors.grey, fontFamily: 'Comfortaa')),
+                const SizedBox(height: 30.0),
+                Text("Rooms", style: TextStyle(fontSize: 15.0, color: Colors.grey[300], fontFamily: 'Comfortaa')),
+                const SizedBox(height: 10.0),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      _makeRoomEl("My Room", true),
+                      const SizedBox(width: 8.0),
+                      _makeRoomEl("Living Room", false),
+                      const SizedBox(width: 8.0),
+                      _makeRoomEl("Bath Room", false),
+                      const SizedBox(width: 8.0),
+                      _makeRoomEl("Entrance", false),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 50.0),
+                Text("Devices", style: TextStyle(fontSize: 15.0, color: Colors.grey[300], fontFamily: 'Comfortaa')),
               ],
             ),
           ),
@@ -52,4 +69,33 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
     );
   }
+}
+
+/* --- Widgets --- */
+
+/// Make room widget.
+Widget _makeRoomEl(String name, bool selected) {
+  return Container(
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.all(Radius.circular(25.0)),
+      color: selected ? Colors.grey[800] : Colors.transparent,
+      border: selected ? Border.all(
+        color: Colors.grey
+      ) : Border.all(
+        color: Colors.grey,
+        width: 0.3
+      ),
+    ),
+    child: Padding(
+      padding: EdgeInsets.all(13.0),
+      child: Text(
+        name,
+        style: TextStyle(
+          color: Colors.white,
+          fontFamily: 'Comfortaa',
+          fontSize: 13.0
+        ),
+      ),
+    ),
+  );
 }
